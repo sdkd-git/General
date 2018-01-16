cec145ad8bbde8b6a1c4cb14010d232d87c1a860
sdkd-git <saideepkavidi@gmail.com>
2018-01-16 12:18:00 +0530

    updated

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Check is user is running with permission
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+#Add Etcher debian repository
+echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee /etc/apt/sources.list.d/etcher.list
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 379CE192D401AB61
+# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+
+# 3. Update list of available packages
+sudo apt-get update
+# 4. Install Spotify
+sudo apt-get install spotify-client
+apt-get install etcher-electron
--- nagiosupgrade.sh
+++ nagiosupgrade.sh
@@ -11,6 +11,7 @@ fi
 #    real_user=$(whoami)
 #fi
 #Stop Nagios and Web Services
+echo -e "Stopping Services"
 systemctl stop nagios.service
 systemctl stop  apache2.service
 nagiosstat='systemctl status nagios.service'
@@ -23,17 +24,17 @@ echo -e "Downloading Nagios Update"
 wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.3.4.tar.gz
 tar -xzvf nagios-4.3.4* >> /tmp/upgradeng.log
 cd nagios-4.3.4*
-#Configure and install the nagios
+echo "Installing the nagios"
 ./configure --with-command-group=nagcmd >> /tmp/upgradeng.log
 make all >> /tmp/upgradeng.log
 make install >> /tmp/upgradeng.log
 make install-init >> /tmp/upgradeng.log
 make install-commandmode >> /tmp/upgradeng.log
-make install-config >> /tmp/upgradeng.log
+# make install-config >> /tmp/upgradeng.log
 #Start Back Nagios Services
+echo "Starting Services"
 systemctl daemon-reload
-systemctl restart nagios.service
-systemctl restart apache2.service
+systemctl restart nagios.service apache2.service
 # check the errors in nagios configuration
-/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg | tail -c 3
 echo "Installation Completed....!"
