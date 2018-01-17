d9717575b5ac069cea3f9a0a7e258d5617c95fb4
sdkd-git <saideepkavidi@gmail.com>
2018-01-17 18:07:35 +0530

    1

--- vc/aptup.sh
+++ vc/aptup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+
 #Script for auto update/autoremove and dist-upgrade.
 
 apt-get autoremove
